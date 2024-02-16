e592a94ddd2c37feb4cb7a4ad72f625bb5b541da
MoniqueC10-4 <monique.charles13@gmail.com>
2024-02-15 22:16:30 -0500

    Completed Dynamic Product Details Page exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useParams, BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import productsData from '../src/data/products.json';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <Link to='/'>Product List</Link>
+        </nav> 
+        <Routes>
+          <Route path='/' element={<ProductList />} />
+          <Route path='/products/:productId' element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+function ProductList() {
+  return (
+    <div>
+      <h1>Product List</h1>
+      {Object.keys(productsData.products).map((productId) => {
+      const product = productsData.products[productId];
+      return (
+      <div key={productId}>
+        <Link to={`/products/${productId}`}>{product.name}</Link>
+      </div>
+      );
+    })}
     </div>
   );
 }
 
+function ProductDetail() {
+  let { productId } = useParams();
+  const product = productsData.products[productId];
+  return (
+    <div>
+      <h1>Product Detail</h1>
+      <p>{product.name}</p>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+    </div>
+  );
+}
 export default App;
